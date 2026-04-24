1e1929a3da2f3533338b511711301f9a902657ec
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2026-04-24 22:37:45 +0200

    a

--- temp.nix
+++ temp.nix
@@ -1,19 +1,19 @@
-{ pkgs, lib, ... }:
 let
   candy-icons = pkgs.stdenv.mkDerivation {
     pname = "candy-icons";
-    version = "latest";
-    src = pkgs.fetchFromGitHub {
-      owner = "EliverLara";
-      repo = "candy-icons";
-      rev = "master";
-      hash = "sha256-TzovzmfrUuaSrtpKCQxyXcih7cKSBhBtMpZLVwY/ScA=";
-    };
-    dontBuild = true;
-    installPhase = "mkdir -p $out/share/icons/candy-icons && cp -r * $out/share/icons/candy-icons/";
+    version = "1.0";
+
+    src = ./candy-icons; # <- MUSS sauber definiert sein
+
+    installPhase = ''
+      mkdir -p $out/share/icons
+      cp -r $src $out/share/icons/candy-icons
+    '';
   };
-in {
-  environment.systemPackages = [ candy-icons ]; # <--- WICHTIG
+in
+{
+  environment.systemPackages = [ candy-icons ];
+
   environment.etc."xdg/kdeglobals".text = lib.mkForce ''
     [Icons]
     Theme[$i]=candy-icons
