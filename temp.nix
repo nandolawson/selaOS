79a50951b1f3789c7ed0106d8d424e252d4ace96
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2026-04-24 23:37:33 +0200

    a

--- temp.nix
+++ temp.nix
@@ -1,7 +1,6 @@
 { pkgs, lib, ... }:
 
 let
-  # 1. Candy Icons (Bleiben wie gehabt)
   candy-icons = pkgs.stdenv.mkDerivation {
     pname = "candy-icons";
     version = "latest";
@@ -15,7 +14,6 @@ let
     installPhase = "mkdir -p $out/share/icons/candy-icons && cp -r * $out/share/icons/candy-icons/";
   };
 
-  # 2. WhiteSur KDE (Das Glas-Theme)
   whitesur-kde = pkgs.stdenv.mkDerivation {
     pname = "whitesur-kde";
     version = "latest";
@@ -23,19 +21,16 @@ let
       owner = "vinceliuice";
       repo = "WhiteSur-kde";
       rev = "master";
-      hash = "sha256-t0bCb1X6BprttUUEcfVqjLskulbFxwXQwUMBn6p8Ho8="; # <-- HIER den Hash aus Schritt 1 einfügen!
+      hash = "sha256-t0bCb1X6BprttUUEcfVqjLskulbFxwXQwUMBn6p8Ho8=";
     };
     dontBuild = true;
     installPhase = ''
-      # Kvantum (Das ist das "Liquid Glass" für Fenster)
       mkdir -p $out/share/Kvantum
       cp -r Kvantum/WhiteSur* $out/share/Kvantum/
 
-      # Plasma Style (Taskleiste)
       mkdir -p $out/share/plasma/desktoptheme
       cp -r Plasma/WhiteSur* $out/share/plasma/desktoptheme/
 
-      # Farbschemata
       mkdir -p $out/share/color-schemes
       cp -r Colors/WhiteSur* $out/share/color-schemes/
     '';
@@ -46,11 +41,10 @@ in
     candy-icons
     whitesur-kde
     libsForQt5.qtstyleplugin-kvantum
-    libsForQt5.kvantum
+    kvantum
   ];
 
   environment.etc = {
-    # Systemweite Icons & Widget-Style
     "xdg/kdeglobals".text = lib.mkForce ''
       [Icons]
       Theme[$i]=candy-icons
@@ -59,19 +53,16 @@ in
       widgetStyle[$i]=kvantum
     '';
 
-    # Kvantum Konfiguration für Glass-Effekt
     "xdg/Kvantum/kvantum.kvconfig".text = lib.mkForce ''
       [General]
       theme=WhiteSurSharp
     '';
 
-    # Plasma Desktop (Taskleiste)
     "xdg/plasmarc".text = lib.mkForce ''
       [Theme]
       name[$i]=WhiteSur
     '';
 
-    # Weichzeichnen (Blur) in KWin erzwingen
     "xdg/kwinrc".text = lib.mkForce ''
       [Plugins]
       blurEnabled=true
