e7e035a08404b91b0a7d9d53979b9b05597d9e8d
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2026-04-24 22:24:58 +0200

    a

--- temp.nix
+++ temp.nix
@@ -1,44 +1,22 @@
 { pkgs, lib, ... }:
-
 let
-  candy-icons = pkgs.stdenv.mkDerivation rec {
+  candy-icons = pkgs.stdenv.mkDerivation {
     pname = "candy-icons";
     version = "latest";
-
     src = pkgs.fetchFromGitHub {
       owner = "EliverLara";
       repo = "candy-icons";
       rev = "master";
-      # Der Hash muss für Nix Flakes im SRI-Format (sha256-...) vorliegen.
-      # Du kannst ihn mit `nix-prefetch-url --unpack https://github.com/EliverLara/candy-icons/archive/master.tar.gz` ermitteln
-      # oder ihn kurz leer lassen, Nix wird dir beim Build den richtigen Hash nennen.
-      hash = "sha256-TzovzmfrUuaSrtpKCQxyXcih7cKSBhBtMpZLVwY/ScA="; 
+      hash = "sha256-TzovzmfrUuaSrtpKCQxyXcih7cKSBhBtMpZLVwY/ScA=";
     };
-
-    installPhase = ''
-      runHook preInstall
-
-      mkdir -p $out/share/icons/candy-icons
-      cp -r * $out/share/icons/candy-icons/
-
-      runHook postInstall
-    '';
-
-    meta = with lib; {
-      description = "Sweet candy gradients icons for KDE Plasma";
-      homepage = "https://github.com/EliverLara/candy-icons";
-      license = licenses.gpl3;
-      platforms = platforms.linux;
-    };
+    dontBuild = true;
+    installPhase = "mkdir -p $out/share/icons/candy-icons && cp -r * $out/share/icons/candy-icons/";
   };
-in
-{
-  # Fügt das Paket zu den installierten System-Paketen hinzu
+in {
   environment.systemPackages = [ candy-icons ];
-
-  # Diese Konfiguration überschreibt die Standard-KDE-Einstellungen systemweit.
-  # "[$i]" macht den Eintrag immutable (Kiosk-Mode), wenn du willst, dass User es nicht ändern können.
-  environment.etc."xdg/kdeglobals".text = ''
+  
+  # mkForce stellt sicher, dass KDE keine Ausreden hat
+  environment.etc."xdg/kdeglobals".text = lib.mkForce ''
     [Icons]
     Theme=candy-icons
   '';
