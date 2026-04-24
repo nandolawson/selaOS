5257c8f7f85e2ab24cd3e7deee00ac1dccae91a0
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2026-04-24 22:31:10 +0200

    a

--- temp.nix
+++ temp.nix
@@ -13,11 +13,9 @@ let
     installPhase = "mkdir -p $out/share/icons/candy-icons && cp -r * $out/share/icons/candy-icons/";
   };
 in {
-  environment.systemPackages = [ candy-icons ];
-  
-  # mkForce stellt sicher, dass KDE keine Ausreden hat
+  environment.systemPackages = [ candy-icons ]; # <--- WICHTIG
   environment.etc."xdg/kdeglobals".text = lib.mkForce ''
     [Icons]
-    Theme=candy-icons
+    Theme[$i]=candy-icons
   '';
 }
