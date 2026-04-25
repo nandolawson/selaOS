9ba27399c6712f1f39da6dc62a1d82cbdab8d787
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2026-04-25 10:03:43 +0200

    a

--- temp.nix
+++ temp.nix
@@ -1,22 +1,2 @@
 { pkgs, lib, ... }:
-let
-  candy-icons = pkgs.stdenv.mkDerivation {
-    pname = "candy-icons";
-    version = "1.0";
-
-    src = ./candy-icons; # <- MUSS sauber definiert sein
-
-    installPhase = ''
-      mkdir -p $out/share/icons
-      cp -r $src $out/share/icons/candy-icons
-    '';
-  };
-in
-{
-  environment.systemPackages = [ candy-icons ];
-
-  environment.etc."xdg/kdeglobals".text = lib.mkForce ''
-    [Icons]
-    Theme[$i]=candy-icons
-  '';
-}
+{}
