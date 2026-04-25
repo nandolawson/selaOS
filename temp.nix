8d259747ad8ce250bd2251631321865acd7be91a
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2026-04-25 08:37:23 +0200

    ö

--- temp.nix
+++ temp.nix
@@ -1,12 +1,3 @@
 { pkgs, lib, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    sweet
-    kdePackages.qtstyleplugin-kvantum
-  ];
-  qt = {
-    enable = true;
-    platformTheme = "kde";
-    style = "kvantum";
-};
 }
