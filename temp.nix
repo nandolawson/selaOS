8d2afe1ed4efe4efa0a6b13f2dc32f3b02579a7f
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2026-04-27 21:45:15 +0200

    Add selaOS hardware detection script

--- temp.nix
+++ temp.nix
@@ -1,2 +1,88 @@
-{ pkgs, lib, ... }:
-{}
+{ pkgs, ... }:
+
+let
+  selaos-logic = pkgs.writeShellApplication {
+    name = "selaOS";
+    runtimeInputs = with pkgs; [
+      coreutils
+      pciutils
+      gnugrep
+      gawk
+      findutils
+      libnotify
+    ];
+    text = ''
+      detect_hardware() {
+        for pair in "AuthenticAMD:Amd" "GenuineIntel:Intel"; do
+          [[ "$(grep -m 1 "vendor_id" /proc/cpuinfo | awk '{print $NF}')" == "''${pair%%:*}" ]] && \
+          HARDWARE+=" cpu''${pair#*:}"
+        done
+        for pair in "1002:gpuAmd" "10de:gpuNvidia" "8086:gpuIntel"; do
+          [[ "$(grep -l "0x0300" /sys/bus/pci/devices/*/class | sed 's/\/class//' | xargs -I {} cat {}/vendor | cut -c 3-6)" == *"''${pair%%:*}"* ]] && \
+          HARDWARE+=" ''${pair#*:}"
+        done
+        for pair in "0106:storageSata" "0108:storageNvme"; do
+          [[ "$(cat /sys/bus/pci/devices/*/class | cut -c 3-6)" == *"''${pair%%:*}"* ]] && HARDWARE+=" ''${pair#*:}"
+        done
+        HARDWARE=$(echo "$HARDWARE" | xargs -n1 | sort -u | xargs)
+        [[ -f /etc/selaos/hardware ]] && [[ "$HARDWARE" != "$(cat /etc/selaos/hardware)" ]] && [[ " $* " == *" --notification "* ]] && notification
+        [[ " $* " == *" --save "* ]] && mkdir -p /etc/selaos && echo "$HARDWARE" > /etc/selaos/hardware
+        if [[ " $* " == *" --show "* ]]
+        then
+          echo "--- Recognized Hardware ---"
+          echo -n "Processor: "
+          [[ "$HARDWARE" == *"cpuAmd"* ]] && echo -n "AMD "
+          [[ "$HARDWARE" == *"cpuIntel"* ]] && echo -n "Intel "
+          echo ""
+          echo -n "Graphics:  "
+          [[ "$HARDWARE" == *"gpuAmd"* ]] && echo -n "AMD "
+          [[ "$HARDWARE" == *"gpuNvidia"* ]] && echo -n "NVIDIA "
+          [[ "$HARDWARE" == *"gpuIntel"* ]] && echo -n "Intel "
+          echo ""
+          echo -n "Storage:   "
+          [[ "$HARDWARE" == *"storageNvme"* ]] && echo -n "NVMe "
+          [[ "$HARDWARE" == *"storageSata"* ]] && echo -n "SATA "
+          echo ""
+          echo "---------------------------"
+        fi
+      }
+
+      notification() {
+        echo "Hardware verändert"
+      }
+# #
+      show_help() {
+          cat << EOF
+Usage: selaos [COMMAND]
+
+Commands:
+  hardware  Run hardware detection
+  help      Show this help
+EOF
+      }
+      case "''${1:-help}" in
+          hardware)
+              shift
+              detect_hardware "$@"
+              ;;
+          help)
+              show_help
+              ;;
+          *)
+              show_help
+              exit 1
+              ;;
+      esac
+    '';
+  };
+
+in
+pkgs.stdenv.mkDerivation {
+  pname = "selaOS";
+  version = "0.1.6";
+  phases = [ "installPhase" ];
+  installPhase = ''
+    mkdir -p $out/bin
+    ln -s ${selaos-logic}/bin/selaos $out/bin/selaos
+  '';
+}
