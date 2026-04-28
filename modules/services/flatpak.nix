d2d9fcef40a4854f1b85e2fbe86e4b84f4b932a7
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2026-04-28 17:56:25 +0200

    a

--- modules/services/flatpak.nix
+++ modules/services/flatpak.nix
@@ -1,14 +1,14 @@
 { lib, ... }:
 {
     services.flatpak = 
-    [
-        {
-            enable = true;
-            remotes =
-                {
-                    name = "flathub";
-                    location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-                }
-        }
-    ];
+    {
+        enable = true;
+        remotes =
+        [
+            {
+                name = "flathub";
+                location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+            }
+        ];
+    }
 }
