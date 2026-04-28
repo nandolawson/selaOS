43f7ff656e69c35dd1bc70ea806afb419d74d1b8
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2026-04-28 17:02:01 +0200

    a

--- modules/default.nix
+++ modules/default.nix
@@ -19,6 +19,7 @@ assert (hardware "gpuAmd" || hardware "gpuIntel" || hardware "gpuNvidia") || thr
       ./boot
       ./hardware.nix
       ./filesystems.nix
+      ./services.nix
     ];
   console.keyMap = "de";
   documentation.nixos = {
@@ -40,66 +41,6 @@ assert (hardware "gpuAmd" || hardware "gpuIntel" || hardware "gpuNvidia") || thr
     experimental-features = [ "nix-command" "flakes" ];
   };
   security.rtkit.enable = true;
-  services = {
-    ananicy = {
-      enable = true;
-      package = pkgs.ananicy-cpp;
-      rulesProvider = pkgs.ananicy-rules-cachyos;
-    };
-    desktopManager.plasma6 = {
-      enable = true;
-      enableQt5Integration = true;
-      notoPackage = pkgs.noto-fonts;
-    };
-    displayManager.sddm = {
-      autoLogin = {
-        minimumUid = 1000;
-        relogin = false;
-      };
-      autoNumlock = false;
-      enable = true;
-      enableHidpi = true;
-      extraPackages = [ ];
-      #package = pkgs.kdePackages.sddm;
-      settings = { };
-      setupScript = "";
-      stopScript = "";
-      #theme = "";
-      wayland = {
-        compositor = "kwin";
-        enable = true;
-      };
-    };
-    flatpak = {
-      enable = true;
-      remotes = lib.mkDefault [
-        {
-          name = "flathub";
-          location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-        }
-      ];
-    };
-    fwupd.enable = true;
-    pipewire = {
-      alsa = {
-        enable = true;
-        support32Bit = true;
-      };
-      enable = true;
-      pulse.enable = true;
-    };
-    xserver = {
-      exportConfiguration = true;
-      videoDrivers =
-        (lib.optionals (hardware "gpuAmd") [ "amdgpu" ])
-        ++ (lib.optionals (hardware "gpuNvidia") [ "nvidia" ])
-        ++ [ "modesetting" ];
-      xkb = {
-        layout = "de";
-        variant = "";
-      };
-    };
-  };
   swapDevices = [
     {
       device = "/swap/swapfile";
