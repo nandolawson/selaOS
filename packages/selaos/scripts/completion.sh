25b8f98c008434c3b8e60a3468e26c9f83258fce
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2026-04-28 20:51:17 +0200

    a

--- packages/selaos/scripts/completion.sh
+++ packages/selaos/scripts/completion.sh
@@ -1,23 +1,19 @@
 completions() {
-    local cur prev opts
+local cur prev
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-    # 1. Level: Die Hauptbefehle
     case "$prev" in
         selaos)
-            opts="hardware update help"
-            COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+            COMPREPLY=( $(compgen -W "hardware update help" -- "$cur") )
             return 0
             ;;
         hardware)
-            # 2. Level: Optionen für hardware
-            opts="--show --save --notification"
-            COMPREPLY=( $(compgen -W "$opts" -- "$cur") )
+            COMPREPLY=( $(compgen -W "--show --save --notification" -- "$cur") )
             return 0
             ;;
-        update)
+        *)
             return 0
             ;;
     esac
