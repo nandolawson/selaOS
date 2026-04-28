420d4a7481008cb07050074a269c4eeb1712ea07
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2026-04-29 01:54:35 +0200

    a

--- modules/boot/initrd/network/default.nix
+++ modules/boot/initrd/network/default.nix
@@ -1,12 +1,12 @@
 { config, ... }:
 {
+    imports = [
+      ./ifstate.nix
+      ./openvpn.nix
+      ./ssh.nix
+      ./udhcpc.nix
+    ];
     boot.initrd.network = {
-      imports = [
-        ./ifstate.nix
-        ./openvpn.nix
-        ./ssh.nix
-        ./udhcpc.nix
-      ];
         enable = false;
         flushBeforeStage2 = !config.boot.initrd.systemd.enable;
         postCommands = "";
