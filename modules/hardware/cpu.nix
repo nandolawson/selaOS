1c93dafb515112c4854042caa5c70e99fb8bf490
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2026-04-28 17:53:11 +0200

    a

--- modules/hardware/cpu.nix
+++ modules/hardware/cpu.nix
@@ -1,11 +1,13 @@
 { lib, hardware, ... }:
 
-hardware.cpu = {
-    amd = lib.mkIf (hardware "cpuAmd") {
-        updateMicrocode = config.hardware.enableRedistributableFirmware;
-    };
-    intel = lib.mkIf (hardware "cpuIntel") {
-        npu.enable = false;
-        updateMicrocode = config.hardware.enableRedistributableFirmware;
-    };
+{
+    hardware.cpu = {
+        amd = lib.mkIf (hardware "cpuAmd") {
+            updateMicrocode = config.hardware.enableRedistributableFirmware;
+        };
+        intel = lib.mkIf (hardware "cpuIntel") {
+            npu.enable = false;
+            updateMicrocode = config.hardware.enableRedistributableFirmware;
+        };
+    }
 }
