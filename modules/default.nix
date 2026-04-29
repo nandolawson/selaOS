fb5c8cd689fad968803ece87c7736641dc2a0e61
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2026-04-29 17:56:00 +0200

    a

--- modules/default.nix
+++ modules/default.nix
@@ -50,10 +50,6 @@ assert (configuration.hardware "gpuAmd" || configuration.hardware "gpuIntel" || 
   environment.etc."flatpak/overrides/global".text = ''
     [Context]
     filesystems=home;
-    [Session Bus Policy]
-    org.freedesktop.Settings=talk
-    org.gtk.vfs=talk
-    org.gtk.vfs.*=talk
   '';
   programs.bash.completion.enable = true;
 }
