911134bed71f1804bd82cf2e62a81a866e1c55c8
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2026-04-30 00:05:57 +0200

    a

--- temp.nix
+++ temp.nix
@@ -1,9 +1,9 @@
 { lib, settings, ... }:
 
 {
-  environment.etc = lib.listToAttrs (map (name: {
-    name = "xdg/autostart/kcm_${name}.desktop";
-    value = { text = "NoDisplay=true"; };
-  }) settings.removedPlasma6Settings);
+#  environment.etc = lib.listToAttrs (map (name: {
+#    name = "xdg/autostart/kcm_${name}.desktop";
+#    value = { text = "NoDisplay=true"; };
+#  }) settings.removedPlasma6Settings);
   environment.etc."xdg/autostart/kcm_gamecontroller.desktop".text = ''NoDisplay=true'';
 }
