ad2ba770b299b6f9a3cef0ef367cf6ee35c2dce6
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2026-04-30 00:08:21 +0200

    a

--- temp.nix
+++ temp.nix
@@ -5,5 +5,5 @@
 #    name = "xdg/autostart/kcm_${name}.desktop";
 #    value = { text = "NoDisplay=true"; };
 #  }) settings.removedPlasma6Settings);
-  environment.etc."xdg/autostart/kcm_gamecontroller.desktop".text = ''NoDisplay=true'';
+  environment.etc."xdg/autostart/kcm_gamecontroller.desktop".text = ''[Desktop Entry] Name=Game Controller NoDisplay=true Type=Service X-KDE-ServiceTypes=KCModule'';
 }
