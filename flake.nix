7f02e530a423386ebee11035958a6bf883f5f626
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2026-05-01 16:56:31 +0200

    a

--- flake.nix
+++ flake.nix
@@ -1,34 +1,20 @@
 {
+  description = "selaOS";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   };
-
-  outputs =
-    {
-      nixpkgs,
-      self,
-      ...
-    }:
-    let
-      name = "selaOS";
-      version = "1.0";
-    in
-    {
-      nixosConfigurations.x86_64 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          settings = builtins.fromTOML (builtins.readFile ./settings.toml);
-          inherit self name version;
-          configuration = {
-            branch = let
-              envBranch = builtins.getEnv "BRANCH";
-            in
-              if envBranch == "release" || envBranch == "insider" || envBranch == "developer"
-              then envBranch
-              else "release";
-            hardware = flag: builtins.match ".*${flag}.*" (builtins.getEnv "HARDWARE") != null;
-          };
-        };
+  outputs = {
+    nixpkgs,
+    self,
+    ...
+  }:
+  let
+    name = "selaOS";
+    version = "1.0";
+  in
+  {
+    nixosConfigurations = {
+      x86_64 = nixpkgs.lib.nixosSystem {
         modules = [
           ./modules
           ./packages
@@ -40,6 +26,23 @@
             }:
           )
         ];
+        specialArgs = {
+          configuration = {
+            branch = let
+            envBranch = builtins.getEnv "BRANCH";
+            in
+              if envBranch == "release" || envBranch == "insider" || envBranch == "developer"
+              then envBranch
+              else "release";
+            hardware = flag: builtins.match ".*${flag}.*" (builtins.getEnv "HARDWARE") != null;
+          };
+          inherit self name version;
+          settings = builtins.fromTOML (
+            builtins.readFile ./settings.toml
+          );
+        };
+        system = "x86_64-linux";
       };
     };
+  };
 }
