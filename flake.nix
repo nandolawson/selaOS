a5cd95f7d1c6720915b7e1978a8b43aff689f6bd
Nando Lawson <37831410+nandolawson@users.noreply.github.com>
2026-05-03 00:52:49 +0200

    a

--- flake.nix
+++ flake.nix
@@ -4,62 +4,16 @@
   inputs = {
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = {
-    nixpkgs-stable,
-    nixpkgs-unstable,
-    self,
-    ...
-  }:
-  let
-    name = "selaOS";
-    version = "1.0";
-    system = "x86_64-linux";
-    
-    # Unstable pkgs für die App-Library
-    pkgs-unstable = import nixpkgs-unstable {
-      inherit system;
-      config.allowUnfree = true;
-    };
-  in
-  {
-    # System-Konfiguration
-    nixosConfigurations.x86_64 = nixpkgs-stable.lib.nixosSystem {
-      inherit system;
-      modules = [
-        ./modules
-        ./packages
-        ./temp.nix
-        ({ lib, ... }: { system.stateVersion = "25.11"; })
+  outputs = inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      imports = [
+        ./nixosConfigurations.nix
+        ./packages.nix
       ];
-      specialArgs = {
-        configuration = {
-          branch = let
-            envBranch = builtins.getEnv "BRANCH";
-          in
-            if envBranch == "developer" || envBranch == "insider" || envBranch == "release"
-            then envBranch
-            else "release";
-          
-          hardware = flag: builtins.match ".*${flag}.*" (
-            builtins.getEnv "HARDWARE"
-          ) != null;
-        };
-        inherit name self version;
-        settings = builtins.fromTOML (builtins.readFile ./settings.toml);
-      };
     };
-
-    # App-Store / Packages (Direkt für x86_64-linux ohne Utils-Wrapper)
-    packages.${system} = pkgs-unstable.lib.mapAttrs' (
-      name: type: pkgs-unstable.lib.nameValuePair (
-        pkgs-unstable.lib.removeSuffix ".nix" name
-      ) (
-        import (./applications + "/${name}") {
-          pkgs = pkgs-unstable;
-        }
-      )
-    ) (builtins.readDir ./applications);
-  };
 }
